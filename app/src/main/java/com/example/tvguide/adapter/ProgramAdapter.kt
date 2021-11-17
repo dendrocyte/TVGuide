541c324ddd5202aec73b1a57b7b4fecfb013c07f
tina <tina.lu@noovo.co>
2021-11-17 15:32:16 +0800

    Add and Rafactor UI Design of No Program
    1.Add threshold of 'No Program' shown
    -too small width is not well-performed 'No Program' text
    2.Refactor 'No Program'’s background color
    Original color is too bright. if too many 'No Program' frame, it will be too crowd.
    Therefore, make it be similar to the dark background, make text little lighten
    -mask also affect the background color

--- app/src/main/java/com/example/tvguide/adapter/ProgramAdapter.kt
+++ app/src/main/java/com/example/tvguide/adapter/ProgramAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.tvguide.adapter
 
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
@@ -27,6 +29,9 @@ import com.example.tvguide.toHHmm
 class ProgramAdapter(data : List<TVScheduleModel>)
     : BaseQuickAdapter<TVScheduleModel, BaseViewHolder>(R.layout.single_live_program, data.toMutableList()) {
 
+    //讓節目時間間隔小於200，就不顯示 "No Program"
+    val THRESHOLD_NO_PG_TXT_SHOW = 150
+
     init {
         /*addChildClickViewIds 不能放在convert()*/
         addChildClickViewIds(R.id.iVmask)
@@ -41,12 +46,21 @@ class ProgramAdapter(data : List<TVScheduleModel>)
 
         helper.getView<TextView>(R.id.tVmsg).visibility =
             if(item.program != null) View.GONE
+            else if (param.width <= THRESHOLD_NO_PG_TXT_SHOW) View.GONE
             else View.VISIBLE
 
         helper.getView<ImageView>(R.id.iVthumbnail).apply {
+            alpha = if (item.program == null) 0.6f else 1f
             Glide.with(context)
                 .load(item.program?.thumbnail)
                 .into(this)
+        }
+
+        helper.getView<ImageView>(R.id.iVmask).apply {
+            setBackgroundColor(
+                if (item.program == null) Color.TRANSPARENT
+                else ContextCompat.getColor(context, R.color.mask)
+            )
         }
 
         helper.getView<TextView>(R.id.tVtitle).text =
